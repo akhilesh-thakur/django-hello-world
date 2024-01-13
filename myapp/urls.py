42719703db5da5daba762179c481401ed693bdc4
Akhilesh <53186185+akhilesh-thakur@users.noreply.github.com>
2024-01-13 11:36:07 +0530

    Create first route  (Home)

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', views.index, name='home'),
 ]
