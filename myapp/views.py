c495f7d1db3e317ea1c786b25309fa33f08d6762
Akhilesh <53186185+akhilesh-thakur@users.noreply.github.com>
2024-01-13 11:52:37 +0530

    Add basic routes

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("How are you my friend?")
+    return HttpResponse("Home Page")
+
+def about(request):
+    return HttpResponse("This is About Page")
+
+def contact(request):
+    return HttpResponse("Contact Page")
+
+def services(request):
+    return HttpResponse("Our Services Page")
 
 # Create your views here.
