123e92c4958bf58430ccef137000937470f023db
Akhilesh <53186185+akhilesh-thakur@users.noreply.github.com>
2024-01-15 11:36:27 +0530

    Create admin super user and change the Admin Panel title.

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Napensea Road Bakery Admin"
+admin.site.site_title = "Napensea Road Bakery Admin Portal"
+admin.site.index_title = "Welcome to Napensea Road Bakery"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls'))
